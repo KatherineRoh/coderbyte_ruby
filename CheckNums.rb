6220b0122db3a47d6b613658608bc3c84f347561
Kyeongmin Katherine Roh <KatherineRoh@users.noreply.github.com>
2015-07-31 19:35:37 +0900

    Create CheckNums.rb

---
+++ CheckNums.rb
@@ -0,0 +1,16 @@
+def CheckNums(num1,num2)
+
+  if num1>num2
+    return ture
+    elsif num1<num2
+    return false
+  else return -1
+  end
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+CheckNums(STDIN.gets)           
+
+#1점이 나왔어요. 이유는 몰라요.
